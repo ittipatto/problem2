d1fde746e90a86ca77ec15f50a446773bbe6d305
ittipatto <ittipat.to@kkumail.com>
2022-08-09 20:34:53 +0700

    'master'

--- scripts/main.js
+++ scripts/main.js
@@ -5,31 +5,6 @@ var max = 0;
 var list;
 const listn = [];
 var i = 0;
-// loopVar:for (;;) {
-//     var text = prompt('Enter a positive integer (a negative integer to quit)');
-//     Int:for(;;){
-//         if (/^[-+]?(\d+|Infinity)$/.test(text)) break Int;
-//         else continue loopVar;
-//     }
-//     var n = Number(text);
-//     // problem
-//     if (!isNaN(n)&&n>0) {
-//         list.push(n);
-//         continue loopVar}
-//         // 
-//     else if(!isNaN(n)&&n<0) break loopVar;
-// }
-
-// function showlist(list) {
-//     if (list.length == 0) {
-//         alert('For the list that is empty, the average is ' + ansAvg + ' the minimum is ' + min + ' and the maximum is ' + max);
-//     } else {
-//         avg(list)
-//         minimum(list)
-//         maximum(list)
-//         alert('For the list ' + list + ' the average is ' + ansAvg + ' the minimum is ' + min + ' and the maximum is ' + max);
-//     }
-// }
 
 function readInput() {
     loopVar: for (; ;) {
@@ -76,8 +51,6 @@ function maximum(list) {
     max = Math.max.apply(Math, list);
     return max;
 }
-
-// showlist(list)
 
 list = readInput();
 displayStats(list);
